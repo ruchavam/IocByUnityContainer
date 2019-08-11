4d086a870f1b34ba158db546c6fb24c45fd51eaa
ruchavam <52919623+ruchavam@users.noreply.github.com>
2019-08-12 00:18:45 +0530

    Add files via upload

---
+++ IOCbyUnityContainerAssignment/IOCbyUnityContainerAssignment/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Unity;
+
+namespace Client
+{
+    public interface IEmployee  
+    {  
+    }  
+    public class Employee : IEmployee  
+    {  
+        private ICompany _Company;  
+        [Dependency]  
+        public ICompany Company  
+        {  
+            get { return _Company; }  
+            set { _Company = value; }  
+        }  
+        public void DisplaySalary()  
+        {  
+            _Company.ShowSalary();  
+        }  
+    }  
+  
+    public interface ICompany  
+    {  
+        void ShowSalary();  
+    }  
+    public class Company : ICompany  
+    {  
+        public void ShowSalary()  
+        {  
+            Console.WriteLine("Your salary is 40 K");  
+        }  
+    }  
+  
+    class Program  
+    {  
+        static void Main(string[] args)  
+        {  
+            IUnityContainer unitycontainer = new UnityContainer();  
+            unitycontainer.RegisterType<ICompany, Company>();  
+  
+            Employee emp = unitycontainer.Resolve<Employee>();  
+            emp.DisplaySalary();  
+  
+  
+            Console.ReadLine();  
+        }  
+    }  
+}  
